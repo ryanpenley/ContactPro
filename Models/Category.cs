2c68767d74490bb35c9001c3d140ea5d49554e1f
Ryan Penley <ryanpenley@gmail.com>
2023-01-31 10:37:36 -0500

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactPro.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? AppUserId { get; set; }
+
+        // Navigation Properties
+        public virtual AppUser? AppUser { get; set; }
+
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,76 @@
+﻿using ContactPro.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactPro.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? AppUserId { get; set; }
+
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and max {1} characters long.", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and max {1} characters long.", MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        [Display(Name = "Birthday")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string? Address1 { get; set; }
+
+        [Display(Name = "Address 2")]
+        public string? Address2 { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+
+        [Required]
+        public States State { get; set; }
+
+      
+        [Display(Name = "Zip Code")]
+        [DataType(DataType.PostalCode)]
+        public int ZipCode { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+        public byte[]? ImageData { get; set; }
+        public string? ImageType { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+        
+
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; }
+
+        // Navigation Properties
+
+        public virtual AppUser? AppUser { get; set; }
+        
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+    }
+}
