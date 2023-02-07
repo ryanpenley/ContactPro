5439b8796343ecdc01dd78c9f08ee0b48b0e6c32
Ryan Penley <ryanpenley@gmail.com>
2023-02-07 16:17:54 -0500

    got demo login working

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = DataUtility.GetConnectionString(builder.Configuration) ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -32,6 +33,13 @@ builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailS
 builder.Services.AddMvc();
 
 var app = builder.Build();
+
+
+var scope = app.Services.CreateScope();
+
+await DataUtility.ManageDataAsync(scope.ServiceProvider);
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
