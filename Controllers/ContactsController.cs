8610504bcd5d63b1f5974739a6468d0b911efe5d
Ryan Penley <ryanpenley@gmail.com>
2023-02-09 17:06:58 -0500

    modified contacts controller

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -227,8 +227,6 @@ namespace ContactPro.Controllers
         }
 
         // POST: Contacts/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
 
